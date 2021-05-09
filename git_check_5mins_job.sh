fbcb9020dc5893bb7bed17473053b5a62f242fea
Avishek <avi9800@gmail.com>
2021-05-09 03:13:35 -0700

    Jenkins final assignment

--- git_check_5mins_job.sh
+++ git_check_5mins_job.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 
-if [ "$(git log --oneline --since="5 minutes ago")" == "" ]  
+if [ -z "$(git log --oneline --since="5 seconds ago")" ]  
 then
-    echo "No commit"
-else
-    echo "Commit"
+    curl -u avi9800:1174829b8dc1ef641fc6bfd3485f8a4b43 http://127.0.0.1:8080/job/build_triggered/build?token=12345
 fi
+
+
+#api code = jenkins_commit_trigger:1174829b8dc1ef641fc6bfd3485f8a4b43
